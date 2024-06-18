74c6f837efe923a0ac7cd3935d597c5d2f032b83
salahelfatimi <salahfatimi16@gmail.com>
2024-06-18 19:24:32 +0100

    xadd

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -31,9 +31,10 @@ export default function Home() {
                     costume your carpet
                 </button>
             </div>
-            <video className="absolute z-10 w-auto brightness-90 min-w-full min-h-full max-w-none" loop muted autoPlay>
-                <source src="video-market.mp4" type="video/mp4" />
+            <video className="absolute z-10 w-auto brightness-90 min-w-full min-h-full max-w-none" loop muted autoPlay preload="none">
+                <source src="videos/video-market.mp4" type="video/mp4" />
             </video>
+            
             {/* <audio className="z-40" ref={audioRef} loop autoPlay={true}>
                 <source src="/music.mp3" type="audio/mp3" />
             </audio>
